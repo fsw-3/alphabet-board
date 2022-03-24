54bec7bad757cc8ab5c95227241722944d07d90c
bobbyrizkinh15 <bobbyrizki15@gmail.com>
2022-03-24 20:19:36 +0700

    add function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function alphabet(num) {
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
+function twoDimensionArray(num) {
+    let arr = [];
+
+    // creating two dimensional array
+    for (let i = 0; i < a; i++) {
+        for (let j = 0; j < b; j++) {
+            arr[i] = [];
+        }
     }
 }
+
+const x = a;
+const y = 3;
+
+const result = twoDimensionArray(x, y);
+console.log(result);
