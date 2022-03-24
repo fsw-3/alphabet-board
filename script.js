7e4f216b01dbd91e43ee59a6d5973847ae80bd10
Muhammad Mufid Ridho <muhammadmufidridho77@gmail.com>
2022-03-24 20:20:55 +0700

    alpha

---
+++ script.js
@@ -0,0 +1,5 @@
+function al(pha,bet) {
+    let alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+}
+
+// maaf ketinggalan tadi kak abis dari masjid
