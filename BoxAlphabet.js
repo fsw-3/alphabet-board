47cdb4a1e3b23699bfbdf0e3183c9acbf312bb3b
Fandi02 <fandi_18520012@stimata.ac.id>
2022-03-24 20:22:10 +0700

    fetch: BoxAlphabet (gagal)

---
+++ BoxAlphabet.js
@@ -0,0 +1,13 @@
+function boxOfAlphabet(baris, kolom) {
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var result = "";
+    for (let i = 0; i < baris; i++) {
+        for (let j = 0; j < kolom; j++) {
+            
+        }
+        
+    }
+    return result;
+}
+
+console.log(boxOfAlphabet(3,4));
