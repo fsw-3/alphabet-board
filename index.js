fa0bff294347e42e79d05c259cf32b9e30bca126
RizkiB12 <rbagus306@gmail.com>
2022-03-24 20:51:59 +0700

    saya akan coba lagi,mohon bantuan nya selalu

---
+++ index.js
@@ -0,0 +1,17 @@
+function alpha(param1,param2) {
+
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","z"];
+let result =[]
+
+for (let i = 0; i < param1;i++){
+     for (let j = 0; j< param2;j++) {
+        result.push(alphabet[i]+alphabet[j]);
+        
+    }
+}
+
+}
+
+alpha(2,2);
+
+//masih saya coba coba lagi
