3d90aaaaf1857c10a5a08141c5ba7be53970e148
SatrioBayu <sbayu875@gmail.com>
2022-03-24 19:48:17 +0700

    Menambah file js yang berisi penyelesaian soal

---
+++ alphabet-board.js
@@ -0,0 +1,66 @@
+const boxOfAlphabet = (tinggi, lebar) => {
+  // Cara 1
+  // Declare the alphabet
+  //   let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  //   let alph = 0;
+  //   //   Declare First Dimension Array
+  //   let result = [];
+
+  //   for (let i = 0; i < tinggi; i++) {
+  //     //   Declare Second Dimension Array
+  //     let arr = [];
+  //     for (let j = 0; j < lebar; j++) {
+  //       arr.push(alphabet[alph++]);
+  //       //   Reset the alph to 0 so it be from 'a' again
+  //       if (alph === alphabet.length) {
+  //         alph = 0;
+  //       }
+  //     }
+  //     // Push Array arr to Array result so it will be 2 dimension array (array inside array)
+  //     result.push(arr);
+  //   }
+
+  //   Cara 2
+  let asciiCode = 97;
+  let result = [];
+
+  for (let i = 0; i < tinggi; i++) {
+    //   Declare Second Dimension Array
+    let arr = [];
+    for (let j = 0; j < lebar; j++) {
+      arr.push(String.fromCharCode(asciiCode++));
+      //   Reset the alph to 0 so it be from 'a' again
+      if (asciiCode > 122) {
+        asciiCode = 97;
+      }
+    }
+    // Push Array arr to Array result so it will be 2 dimension array (array inside array)
+    result.push(arr);
+  }
+
+  return result;
+};
+
+console.log(boxOfAlphabet(2, 2));
+// output
+// [
+//   ['a', 'b'],
+//   ['c', 'd']
+// ]
+console.log(boxOfAlphabet(3, 4));
+// output
+// [
+//   ['a', 'b', 'c', 'd'],
+//   ['e', 'f', 'g', 'h'],
+//   ['i', 'j', 'k', 'l']
+// ]
+console.log(boxOfAlphabet(6, 5));
+// output
+// [
+//   ['a', 'b', 'c', 'd', 'e'],
+//   ['f', 'g', 'h', 'i', 'j'],
+//   ['k', 'l', 'm', 'n', 'o'],
+//   ['p', 'q', 'r', 's', 't'],
+//   ['u', 'v', 'w', 'x', 'y'],
+//   ['z', 'a', 'b', 'c', 'd']
+// ]
